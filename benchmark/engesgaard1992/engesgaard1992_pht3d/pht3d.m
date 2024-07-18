1d49da94f153b4038b2ee345bd24d94205b2d501
portega <portega@intera.com>
2024-07-18 16:18:45 +0800

    (refactor: benchmark) changed file-folder organization and names

--- benchmark/engesgaard1992/engesgaard1992_pht3d/pht3d.m
+++
@@ -1,8 +0,0 @@
-C_4 = load('PHT3D001.ACN');
-Ca = load('PHT3D002.ACN');
-Cl = load('PHT3D003.ACN');
-Mg = load('PHT3D004.ACN');
-pH = load('PHT3D005.ACN');
-pe = load('PHT3D006.ACN');
-Calcite = load('PHT3D007.ACN');
-Dolomite = load('PHT3D008.ACN');
