5c1650fe92f2f2f1dc983e959040fa97ec9abc0d
portega <portega@intera.com>
2024-07-30 16:21:44 +0800

    (add:autotest) add test03. 2d model and init for pytest

--- autotest/autotest.py
+++ autotest/autotest.py
@@ -262,7 +262,249 @@ def build_mf6_1d_injection_model(mup3d, nper, tdis_rc, length_units, time_units,
     
     return sim
 
-def test_01(prefix = 'test01'):
+def build_mf6_2d_model(mup3d, nper, tdis_rc, length_units, time_units, nlay, nrow, ncol, delr, delc,
+                                 top, botm, chdspd, prsity, k11, k33, dispersivity, disp_tr_vert,icelltype, hclose,
+                                 strt, rclose, relax, nouter, ninner):
+
+    #####################        GWF model           #####################
+    gwfname = 'gwf'
+    sim_ws = mup3d.wd
+    sim = flopy.mf6.MFSimulation(sim_name=mup3d.name, sim_ws=sim_ws, exe_name='mf6')
+
+    # Instantiating MODFLOW 6 time discretization
+    flopy.mf6.ModflowTdis(sim, nper=nper, perioddata=tdis_rc, time_units=time_units)
+
+    # Instantiating MODFLOW 6 groundwater flow model
+    gwf = flopy.mf6.ModflowGwf(
+        sim,
+        modelname=gwfname,
+        save_flows=True,
+        model_nam_file=f"{gwfname}.nam",
+    )
+
+    # Instantiating MODFLOW 6 solver for flow model
+    imsgwf = flopy.mf6.ModflowIms(
+        sim,
+        complexity="complex",
+        print_option="SUMMARY",
+        outer_dvclose=hclose,
+        outer_maximum=nouter,
+        under_relaxation="NONE",
+        inner_maximum=ninner,
+        inner_dvclose=hclose,
+        rcloserecord=rclose,
+        linear_acceleration="CG",
+        scaling_method="NONE",
+        reordering_method="NONE",
+        relaxation_factor=relax,
+        filename=f"{gwfname}.ims",
+    )
+    sim.register_ims_package(imsgwf, [gwf.name])
+
+    # Instantiating MODFLOW 6 discretization package
+    dis = flopy.mf6.ModflowGwfdis(
+        gwf,
+        length_units=length_units,
+        nlay=nlay,
+        nrow=nrow,
+        ncol=ncol,
+        delr=delr,
+        delc=delc,
+        top=top,
+        botm=botm,
+        idomain=np.ones((nlay, nrow, ncol), dtype=int),
+        filename=f"{gwfname}.dis",
+    )
+    dis.set_all_data_external()
+
+    # Instantiating MODFLOW 6 node-property flow package
+    npf = flopy.mf6.ModflowGwfnpf(
+        gwf,
+        save_flows=True,
+        save_saturation = True,
+        icelltype=icelltype,
+        k=k11,
+        k33=k33,
+        save_specific_discharge=True,
+        filename=f"{gwfname}.npf",
+    )
+    npf.set_all_data_external()
+    # sto = flopy.mf6.ModflowGwfsto(gwf, ss=1e-6, sy=0.25)
+
+    # Instantiating MODFLOW 6 initial conditions package for flow model
+    flopy.mf6.ModflowGwfic(gwf, strt=strt, filename=f"{gwfname}.ic")
+
+    # Instantiating MODFLOW 6 constant head package
+    chd = flopy.mf6.ModflowGwfchd(
+        gwf,
+        maxbound=len(chdspd),
+        stress_period_data=chdspd,
+        auxiliary=mup3d.components,
+        save_flows=False,
+        pname="CHD",
+        filename=f"{gwfname}.chd",
+    )
+    chd.set_all_data_external()
+
+    # Instantiating MODFLOW 6 output control package for flow model
+    oc_gwf = flopy.mf6.ModflowGwfoc(
+        gwf,
+        head_filerecord=f"{gwfname}.hds",
+        budget_filerecord=f"{gwfname}.cbb",
+        headprintrecord=[("COLUMNS", 10, "WIDTH", 15, "DIGITS", 6, "GENERAL")],
+        saverecord=[("HEAD", "ALL"), ("BUDGET", "ALL")],
+        printrecord=[("HEAD", "LAST"), ("BUDGET", "LAST")],
+    )
+    
+    #####################           GWT model          #####################
+    for c in mup3d.components:
+        print(f'Setting model for component: {c}')
+        gwtname = c
+        
+        # Instantiating MODFLOW 6 groundwater transport package
+        gwt = flopy.mf6.MFModel(
+            sim,
+            model_type="gwt6",
+            modelname=gwtname,
+            model_nam_file=f"{gwtname}.nam"
+        )
+
+        # create iterative model solution and register the gwt model with it
+        print('--- Building IMS package ---')
+        imsgwt = flopy.mf6.ModflowIms(
+            sim,
+            print_option="SUMMARY",
+            outer_dvclose=hclose,
+            outer_maximum=nouter,
+            under_relaxation="NONE",
+            inner_maximum=ninner,
+            inner_dvclose=hclose,
+            rcloserecord=rclose,
+            linear_acceleration="BICGSTAB",
+            scaling_method="NONE",
+            reordering_method="NONE",
+            relaxation_factor=relax,
+            filename=f"{gwtname}.ims",
+        )
+        sim.register_ims_package(imsgwt, [gwt.name])
+
+        print('--- Building DIS package ---')
+        dis = gwf.dis
+
+        # create grid object
+        dis = flopy.mf6.ModflowGwtdis(
+            gwt,
+            length_units=length_units,
+            nlay=nlay,
+            nrow=nrow,
+            ncol=ncol,
+            delr=delr,
+            delc=delc,
+            top=top,
+            botm=botm,
+            idomain=np.ones((nlay, nrow, ncol), dtype=int),
+            filename=f"{gwtname}.dis",
+        )
+        dis.set_all_data_external()
+
+         
+        ic = flopy.mf6.ModflowGwtic(gwt, strt=mup3d.sconc[c], filename=f"{gwtname}.ic")
+        ic.set_all_data_external()
+
+        # cncspd = {0: [[(0, 0, col), conc] for col, conc in zip(range(ncol), model.sconc[c][0,0,:])]}
+        cncspd = {0: [[(ly, 0, 0), mup3d.sconc[c][ly,0,0]] for ly in range(3,nlay)]}
+
+        # print(cncspd)
+        cnc = flopy.mf6.ModflowGwtcnc(gwt,
+                                        # maxbound=len(cncspd),
+                                        stress_period_data=cncspd,
+                                        save_flows=True,
+                                        print_flows = True,
+                                        pname="CNC",
+                                        filename=f"{gwtname}.cnc",
+                                        )
+        cnc.set_all_data_external()
+        # Instantiating MODFLOW 6 transport source-sink mixing package
+        sourcerecarray = ['chd', 'aux', f'{c}']
+        # sourcerecarray = [()]
+        ssm = flopy.mf6.ModflowGwtssm(
+            gwt, 
+            sources=sourcerecarray, 
+            save_flows=True,
+            print_flows=True,
+
+            filename=f"{gwtname}.ssm"
+        )
+        ssm.set_all_data_external()
+        # Instantiating MODFLOW 6 transport adv package
+        print('--- Building ADV package ---')
+        adv = flopy.mf6.ModflowGwtadv(
+            gwt,
+            scheme="tvd",
+        )
+
+        # Instantiating MODFLOW 6 transport dispersion package
+        alpha_l = np.ones(shape=(nlay, nrow, ncol))*dispersivity  # Longitudinal dispersivity ($m$)
+        ath1 = np.ones(shape=(nlay, nrow, ncol))*dispersivity  # Transverse horizontal dispersivity ($m$)
+        atv = np.ones(shape=(nlay, nrow, ncol))*disp_tr_vert  # Transverse vertical dispersivity ($m$)
+
+        print('--- Building DSP package ---')
+        dsp = flopy.mf6.ModflowGwtdsp(
+            gwt,
+            xt3d_off=True,
+            alh=alpha_l,
+            ath1=ath1,
+            atv = atv,
+            # diffc = diffc,
+            filename=f"{gwtname}.dsp",
+        )
+        dsp.set_all_data_external()
+
+        # Instantiating MODFLOW 6 transport mass storage package (formerly "reaction" package in MT3DMS)
+        print('--- Building MST package ---')
+
+        first_order_decay = None
+
+        mst = flopy.mf6.ModflowGwtmst(
+            gwt,
+            porosity=prsity,
+            first_order_decay=first_order_decay,
+            filename=f"{gwtname}.mst",
+        )
+        mst.set_all_data_external()
+
+        print('--- Building OC package ---')
+
+        # Instantiating MODFLOW 6 transport output control package
+        oc_gwt = flopy.mf6.ModflowGwtoc(
+            gwt,
+            budget_filerecord=f"{gwtname}.cbb",
+            concentration_filerecord=f"{gwtname}.ucn",
+            concentrationprintrecord=[("COLUMNS", 10, "WIDTH", 15, "DIGITS", 10, "GENERAL")
+                                        ],
+            saverecord=[("CONCENTRATION", "ALL"), 
+                        ("BUDGET", "ALL")
+                        ],
+            printrecord=[("CONCENTRATION", "ALL"), 
+                            ("BUDGET", "ALL")
+                            ],
+        )
+
+        # Instantiating MODFLOW 6 flow-transport exchange mechanism
+        flopy.mf6.ModflowGwfgwt(
+            sim,
+            exgtype="GWF6-GWT6",
+            exgmnamea=gwfname,
+            exgmnameb=gwtname,
+            filename=f"{gwtname}.gwfgwt",
+        )
+
+    sim.write_simulation()
+    utils.prep_bins(sim_ws, src_path=os.path.join('..','bin'), get_only=['mf6', 'libmf6'])
+    
+    return sim
+
+def test01(prefix = 'test01'):
 
     '''Test 1: Simple 1D injection test with equilibrium phases'''	
     ### Model params and setup
@@ -358,7 +600,7 @@ def test_01(prefix = 'test01'):
 
     return 
 
-def test_02(prefix = 'test02'):
+def test02(prefix = 'test02'):
     # General
     length_units = "meters"
     time_units = "days"
@@ -472,7 +714,124 @@ def test_02(prefix = 'test02'):
         pass
     return
 
-def test_04(prefix = 'test04'):
+def test03(prefix = 'test03'):
+    length_units = "meters"
+    time_units = "days"
+
+    # Model discretization
+    nlay = 10  # Number of layers
+    Lx = 100 #m
+    ncol = 25 # Number of columns
+    nrow = 1  # Number of rows
+    delr = Lx/ncol #10.0  # Column width ($m$)
+    delc = 1.0  # Row width ($m$)
+    top = 10.  # Top of the model ($m$)
+    # botm = 0.0  # Layer bottom elevations ($m$)
+    zbotm = 0.
+    botm = np.linspace(top, zbotm, nlay + 1)[1:]
+
+    #tdis
+    nper = 1  # Number of periods
+    tstep = 1  # Time step ($days$)
+    perlen = 2000  # Simulation time ($days$)
+    nstp = perlen/tstep #100.0
+    dt0 = perlen / nstp
+    tdis_rc = []
+    tdis_rc.append((perlen, nstp, 1.0))
+
+    #hydraulic properties
+    prsity = 0.35  # Porosity
+    k11 = 1.0  # Horizontal hydraulic conductivity ($m/d$)
+    k33 = k11  # Vertical hydraulic conductivity ($m/d$)
+    strt = np.ones((nlay, nrow, ncol), dtype=float)*10
+    # two chd one for tailings and conc and other one for hds 
+
+    l_hd = 12
+    r_hd = 10
+    strt = np.ones((nlay, nrow, ncol), dtype=float)*10
+    strt[:, 0, 0] = l_hd  # Starting head ($m$)
+
+    chdspd = [[(i, 0, 0), l_hd] for i in range(nlay)] # Constant head boundary $m$
+    chdspd.extend([(i, 0, ncol - 1), r_hd] for i in range(nlay))
+
+
+    #transport
+    dispersivity = 2.5 # Longitudinal dispersivity ($m$)
+    disp_tr_vert = 0.025 # Transverse vertical dispersivity ($m$)
+
+    icelltype = 0  # Cell conversion type
+
+    # Set solver parameter values (and related)
+    nouter, ninner = 300, 600
+    hclose, rclose, relax = 1e-6, 1e-6, 1.0
+
+    solutionsdf = pd.read_csv(os.path.join(dataws,f"{prefix}_solutions.csv"), comment = '#',  index_col = 0)
+
+    # solutions = utils.solution_csv_to_dict(os.path.join(dataws,f"{prefix}_solutions.csv"))
+    solutions = utils.solution_df_to_dict(solutionsdf)
+
+    equilibrium_phases = utils.equilibrium_phases_csv_to_dict(os.path.join(dataws, f'{prefix}_equilibrium_phases.csv'))
+
+    # equlibrium phases is a dictionary with keys as the phase number, values is another dictionary with phase name and an array of saturation indices as element 0 and concentrations as element 1. multiply the concentrations by 2
+    for key, value in equilibrium_phases.items():
+        for k, v in value.items():
+            v[-1] = mf6rtm.concentration_volbulk_to_volwater( v[-1], prsity)
+    #assign solutions to grid
+    sol_ic = np.ones((nlay, nrow, ncol), dtype=int)
+
+    #add solutions to clss
+    solution = mf6rtm.Solutions(solutions)
+    solution.set_ic(sol_ic)
+
+    #create equilibrium phases class
+    equilibrium_phases = mf6rtm.EquilibriumPhases(equilibrium_phases)
+    eqp_ic = np.ones((nlay, nrow, ncol), dtype=int)*1
+    eqp_ic[3:,:,0]= -1 #boundary condation in layer 0 of no eq phases
+    equilibrium_phases.set_ic(eqp_ic)
+
+    #create model class
+    model = mf6rtm.Mup3d(prefix,solution, nlay, nrow, ncol)
+
+    #set model workspace
+    if not os.path.exists(prefix):
+        os.makedirs(prefix)
+    model.set_wd(os.path.join(f'{prefix}'))
+
+    #set database
+    database = os.path.join(databasews, f'pht3d_datab_walter1994.dat')
+    model.set_database(database)
+
+    #include equilibrium phases in model class
+    model.set_equilibrium_phases(equilibrium_phases)
+
+    postfix = os.path.join(dataws, f'{prefix}_postfix.phqr')
+    model.set_postfix(postfix)
+    model.initialize()
+
+    wellchem = mf6rtm.ChemStress('chdtail')
+    sol_spd = [2]
+    wellchem.set_spd(sol_spd)
+    model.set_chem_stress(wellchem)
+
+    for i in range(len(chdspd)):
+        if i<3:
+            chdspd[i].extend(model.chdtail.data[0])
+        else:
+            chdspd[i].extend(np.zeros_like(model.chdtail.data[0]))
+
+    mf6sim = build_mf6_2d_model(model, nper, tdis_rc, length_units, time_units, nlay, nrow, ncol, delr, delc,
+                                 top, botm, chdspd, prsity, k11, k33, dispersivity, disp_tr_vert,icelltype, hclose,
+                                 strt, rclose, relax, nouter, ninner)
+    
+    run_test(prefix, model, mf6sim)
+
+    try:
+        cleanup(prefix)
+    except:
+        pass
+    return 
+
+def test04(prefix = 'test04'):
     '''Test 4: Test 1: Simple 1D injection test with cation exchange from phreeqc'''
     # General
     length_units = "meters"
@@ -583,7 +942,7 @@ def test_04(prefix = 'test04'):
     return 
 
 
-def test_05(prefix = 'test05'):
+def test05(prefix = 'test05'):
     '''Test 5: oxidation with pyrite 1D test
     This tests equilibrum phases, scm, kinetics and exchange    
     '''
